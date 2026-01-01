990988c035b75c6ae8483cb80f899aa76f60b77c
Anubhav <programmingwithanubhav@gmail.com>
2026-01-01 12:07:37 +0530

    Initial Commit

---
+++ Materia/MateriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MateriaApp.swift
+//  Materia
+//
+//  Created by Anubhav Dubey on 01/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MateriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
