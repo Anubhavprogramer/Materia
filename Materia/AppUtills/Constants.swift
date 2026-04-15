b335f2e0c0c422eabbdc2d0db7d90c76dc72bab7
Anubhav <programmingwithanubhav@gmail.com>
2026-04-15 22:40:52 +0530

    Rename app to Materiya and update icons
    Rebrand app from "Materia" to "Materiya": update AppStrings.AppName and set INFOPLIST_KEY_CFBundleDisplayName in the Xcode project. Change PRODUCT_BUNDLE_IDENTIFIER to com.anubhav.materiyaApp in project.pbxproj. Replace old placeholder ChatGPT icon images with new app icon files and add app_logo assets including @2x and @3x variants; update AppIcon and app_logo imageset Contents.json accordingly. Remove deprecated ChatGPT image files.

--- Materia/AppUtills/Constants.swift
+++ Materia/AppUtills/Constants.swift
@@ -9,7 +9,7 @@ import UIKit
 
 struct AppStrings {
     // MARK: - Navigation & Tabs
-    static let AppName = "Materia"
+    static let AppName = "Materiya"
     static let search = "Search"
     static let weight = "Weight"
     static let live = "Live"
